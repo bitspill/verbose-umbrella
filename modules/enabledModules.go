29eafd2126c7a1bf27a8524204d672b29de2c1ef
Jeremiah Buddenhagen <git@bitspill.net>
2020-06-28 23:57:52 -0700

    disable modules

--- modules/enabledModules.go
+++ modules/enabledModules.go
@@ -1,13 +1,13 @@
 package modules
 
 import (
-	_ "github.com/oipwg/oip/modules/alexandriaMedia"
-	_ "github.com/oipwg/oip/modules/aternaLove"
-	_ "github.com/oipwg/oip/modules/flotorizer"
-	_ "github.com/oipwg/oip/modules/historian"
+	// _ "github.com/oipwg/oip/modules/alexandriaMedia"
+	// _ "github.com/oipwg/oip/modules/aternaLove"
+	// _ "github.com/oipwg/oip/modules/flotorizer"
+	// _ "github.com/oipwg/oip/modules/historian"
 	_ "github.com/oipwg/oip/modules/oip"
-	_ "github.com/oipwg/oip/modules/oip041"
-	_ "github.com/oipwg/oip/modules/oip042"
+	// _ "github.com/oipwg/oip/modules/oip041"
+	// _ "github.com/oipwg/oip/modules/oip042"
 	_ "github.com/oipwg/oip/modules/oip5"
-	_ "github.com/oipwg/oip/modules/tZero"
+	// _ "github.com/oipwg/oip/modules/tZero"
 )
